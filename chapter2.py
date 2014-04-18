afc7ef09bc42457279b5e89241472566675df48e
Twice Tshwenyane <twicet@gmail.com>
2014-04-18 09:11:32 +0200

    corrected to_f calls to float()

--- chapter2.py
+++ chapter2.py
@@ -1,12 +1,12 @@
 ############## Page 18 ##############
 chainring = 52                    # number of teeth
 cog = 11
-ratio = chainring / cog.to_f
+ratio = chainring / float(cog)
 print ratio                        # -> 4.72727272727273
 
 chainring = 30
 cog = 27
-ratio = chainring / cog.to_f
+ratio = chainring / float(cog)
 print ratio                        # -> 1.11111111111111
 
 ############## Page 19 ##############
